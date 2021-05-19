c8d62cb1ba8992536720e4662b5a62735fc5fbbc
TaymaaDawood <82846103+TaymaaDawood@users.noreply.github.com>
2021-05-19 11:27:46 +0100

    Create scripts.js
    add a recipe object with an arraies inside

---
+++ scripts/scripts.js
@@ -0,0 +1,43 @@
+/*
+create an onject to hold information for our favorite recipe
+
+OBJECT
+  RECIPETITLE = sting
+  SERVINGS = number
+  INGREDINTS = arry of string
+  DIRECTIONS = array of string
+
+let favoriteRecipe = {
+  recipeTitle='',
+  servings=6,
+  ingredints=['1','2','3'],
+  directions=['step1',' step2'] 
+}
+
+to loop over the INGREDINTS
+  FOR
+    COUNTER =0
+    CONDITION = counter < INGREDINTS>LENGTH
+    COUNTER+1
+
+
+*/
+
+let favoriteRecipe =
+ {
+    recipeTitle:'Kebab',
+    servings :4,
+    ingredints: ['mince meat','salt','spices','Sticks'],
+    directions:
+    ['Put the mince meat in a bowl',
+    ' Put the salt and spices', 
+    'Mix them very well',
+    ] 
+  }
+
+  let ingredints = favoriteRecipe.ingredints;
+  let directions= favoriteRecipe.directions;
+
+  for(counter=0; counter < directions.length; counter ++){
+      console.log(directions[counter]);
+  }
